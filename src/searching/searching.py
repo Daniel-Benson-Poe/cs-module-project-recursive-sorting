d679a94fecc70ec7356a98d9c9fcf4b182f65217
Poenomaly <djbenson741@gmail.com>
2020-08-20 21:36:32 -0600

    Complete module mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,8 +1,25 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    # check if start greater than end
+    if start > end:
+        # return none
+        return -1
+    else:
+        # set midpoint of array
+        midpoint = (start + end) // 2
+        # check if midpoint and target are equal
+        if arr[midpoint] == target:
+            return midpoint
+        # check if midpoint value is less than target        
+        elif arr[midpoint] < target:
+            # recursively run binary search again setting start to midpoint + 1
+            return binary_search(arr, target, midpoint + 1, end)
+        else:
+            # recursively run binary search again setting end to midpoint - 1
+            return binary_search(arr, target, start, midpoint - 1)
 
-
+            
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
 # the target regardless of whether the input array is
@@ -11,4 +28,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
